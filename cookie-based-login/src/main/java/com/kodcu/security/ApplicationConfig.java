dde84aeb36b81dae978a165d9578aff7fdeaef03
AltugAltintas <altuga@gmail.com>
2019-11-13 11:41:49 +0300

    cookie based example is added

---
+++ cookie-based-login/src/main/java/com/kodcu/security/ApplicationConfig.java
@@ -0,0 +1,18 @@
+package com.kodcu.security;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+import javax.security.enterprise.authentication.mechanism.http.CustomFormAuthenticationMechanismDefinition;
+import javax.security.enterprise.authentication.mechanism.http.LoginToContinue;
+
+@CustomFormAuthenticationMechanismDefinition(
+        loginToContinue = @LoginToContinue(
+                loginPage = "/login.xhtml",
+                useForwardToLogin = false
+            )
+)
+@FacesConfig
+@ApplicationScoped
+public class ApplicationConfig {
+}
+
