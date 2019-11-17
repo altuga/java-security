1242f7822768c4c26ba9c0bcb88b225cfa058bb5
AltugAltintas <altuga@gmail.com>
2019-11-17 18:23:10 +0300

    SEC55-J. Ensure that security-sensitive methods are called with validated arguments added

--- rest-client-for-web/src/main/java/com/airhacks/ping/boundary/PingResource.java
+++ rest-client-for-web/src/main/java/com/airhacks/ping/boundary/PingResource.java
@@ -2,14 +2,23 @@ package com.airhacks.ping.boundary;
 
 import com.airhacks.data.Mutable;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
+import java.util.Objects;
 
 /**
  *
  * @author airhacks.com
  */
+@Stateless
 @Path("ping")
 public class PingResource {
 
@@ -33,6 +42,24 @@ public class PingResource {
             result.append(mutable.getArray()[i] +"-");
         }
         return result.toString();
+    }
+
+    @POST
+    public Response newPost(@Context UriInfo info, String data) {
+
+        // Ensure that security-sensitive methods are called with validated arguments
+        // curl -H "Content-Type:application/json"  -X POST -v http://localhost:8080/rest-client-for-web/resources/ping/
+        // curl -H "Content-Type:application/json" -d '{"numberOfSeats":33}' -X POST -v http://localhost:8080/rest-client-for-web/resources/ping/
+
+       // TODO 1
+
+
+        System.out.println(" " + data);
+        URI uri = info.getAbsolutePathBuilder().
+                path(String.valueOf(14)).
+                build();
+        return Response.created(uri).build();
+
     }
 
 }
