9b1cf0ef6fd234f2adc4d205ac9ef5c27f01cc15
AltugAltintas <altuga@gmail.com>
2019-11-30 13:55:55 +0300

    38 solution - 1

--- effective_java/src/main/java/com/kodcu/question38/StopThread.java
+++ effective_java/src/main/java/com/kodcu/question38/StopThread.java
@@ -6,20 +6,30 @@ import java.util.concurrent.TimeUnit;
 * How long would you expect this program to run?
 */
 public class StopThread {
-    private static boolean stopRequested;
+    private static boolean stopRequested = false;
+
+    private static synchronized void requestStop() {
+        stopRequested = true;
+    }
+
+    private static synchronized boolean stopRequested() {
+        return stopRequested;
+    }
 
     public static void main(String[] args)
             throws InterruptedException {
+
         Thread backgroundThread = new Thread(() -> {
             int i = 0;
-            while (!stopRequested)
+            while (!stopRequested())
                 i++;
             System.out.println("Finito");
         });
+
         backgroundThread.start();
 
         TimeUnit.SECONDS.sleep(1);
-        stopRequested = true;
+        requestStop();
 
     }
 }
