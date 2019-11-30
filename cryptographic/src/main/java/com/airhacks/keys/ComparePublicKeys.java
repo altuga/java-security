64a69dbd1180b603aaba420cb04c3ba23664ad2f
AltugAltintas <altuga@gmail.com>
2019-11-30 16:44:25 +0300

    comments are added

--- cryptographic/src/main/java/com/airhacks/keys/ComparePublicKeys.java
+++ cryptographic/src/main/java/com/airhacks/keys/ComparePublicKeys.java
@@ -5,6 +5,11 @@ import java.util.Arrays;
 
 public class ComparePublicKeys {
 
+    @Override
+    public String toString() {
+        return "ComparePublicKeys{}";
+    }
+
     public static void main(String[] args) throws Exception {
         AsymmetricCryptography ac = new AsymmetricCryptography();
         PublicKey publicKey = ac.getPublic("KeyPair/publicKey");
@@ -12,12 +17,14 @@ public class ComparePublicKeys {
 
         PublicKey publicKey2 = ac.getPublic("KeyPair/publicKey");
 
+        // warning
         if (publicKey.equals(publicKey2)) {
             System.out.println("true");
         } else {
             System.out.println("false");
         }
 
+        // correct
         if (Arrays.equals(publicKey.getEncoded(), publicKey2.getEncoded())) {
             System.out.println("true");
         }else {
